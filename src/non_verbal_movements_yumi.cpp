6fc284201b4b06e79dfbe8725e6a0cf0b657e8ee
Rodrigo Carvalho <rp.carvalho@campus.fct.unl.pt>
2023-07-01 20:12:35 +0100

    changed HRI_Interface class and whole program to plan for arm and gripper

--- src/non_verbal_movements_yumi.cpp
+++ src/non_verbal_movements_yumi.cpp
@@ -34,24 +34,24 @@ const double tau = 2 * M_PI;
 class HRI_Interface
 {
 public:
-    HRI_Interface(ros::NodeHandle n, std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface,
+    HRI_Interface(ros::NodeHandle n,
+                  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> arm_mgi,
+                  const moveit::core::JointModelGroup *arm_jmg,
+                  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> gripper_mgi,
+                  const moveit::core::JointModelGroup *gripper_jmg,
                   std::shared_ptr<moveit::planning_interface::PlanningSceneInterface> planning_scene_interface,
-                  const moveit::core::JointModelGroup *joint_model_group,
                   std::shared_ptr<moveit_visual_tools::MoveItVisualTools> visual_tools)
-        : n_(n), move_group_interface_(move_group_interface), planning_scene_interface_(planning_scene_interface), joint_model_group_(joint_model_group), visual_tools_(visual_tools)
+        : n_(n), arm_mgi_(arm_mgi), arm_jmg_(arm_jmg), gripper_mgi_(gripper_mgi), gripper_jmg_(gripper_jmg), planning_scene_interface_(planning_scene_interface), visual_tools_(visual_tools)
     {
-        robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-        kinematic_model_ = robot_model_loader.getModel();
-
         transform_listener = n_.serviceClient<planner::TransformListener>("transform_listener_srv");
     }
 
     void exagerateTrajectory(moveit::planning_interface::MoveGroupInterface::Plan plan, std::string planning_group, Eigen::Vector3d xyz)
     {
-
+        // TODO - Fix kniematic_model
         // REFERENCE: https://github.com/ros-planning/moveit/issues/1556
 
-        robot_state::RobotStatePtr kinematic_state(move_group_interface_->getCurrentState());
+        robot_state::RobotStatePtr kinematic_state(arm_mgi_->getCurrentState());
 
         int num_waypoints = plan.trajectory_.joint_trajectory.points.size(); // gets the number of waypoints in the trajectory
         int mid_waypoint = num_waypoints / 2;
@@ -181,42 +181,42 @@ public:
             previous_joint_values = plan.trajectory_.joint_trajectory.points.at(i).positions;
         }
 
-        move_group_interface_->execute(plan);
+        arm_mgi_->execute(plan);
     }
 
-    void yes_no(std::string target_frame)
+    void attention(std::string target_frame)
     {
 
-        move_group_interface_->setPlannerId("stomp");
+        arm_mgi_->setPlannerId("stomp");
         // Set constraints for arm pose
         // These constraints are meant to set the arm in a V shape (approximately)
         moveit_msgs::Constraints path_constraints;
         // Add joint constraints
         moveit_msgs::JointConstraint joint_constraint;
 
-        joint_constraint.joint_name = move_group_interface_->getActiveJoints().at(0); // "yumi_joint_1_r"
+        joint_constraint.joint_name = arm_mgi_->getActiveJoints().at(0); // "yumi_joint_1_r"
         joint_constraint.position = 125 * (M_PI / 180);
         joint_constraint.tolerance_above = 0.2;
         joint_constraint.tolerance_below = 0.2;
         joint_constraint.weight = 1.0;
         path_constraints.joint_constraints.push_back(joint_constraint);
 
-        joint_constraint.joint_name = move_group_interface_->getActiveJoints().at(1); // "yumi_joint_2_r"
+        joint_constraint.joint_name = arm_mgi_->getActiveJoints().at(1); // "yumi_joint_2_r"
         joint_constraint.position = -115 * (M_PI / 180);
         joint_constraint.tolerance_above = 0.1;
         joint_constraint.tolerance_below = 0.1;
         joint_constraint.weight = 1.0;
         path_constraints.joint_constraints.push_back(joint_constraint);
 
-        joint_constraint.joint_name = move_group_interface_->getActiveJoints().at(3); // "yumi_joint_3_r"
+        joint_constraint.joint_name = arm_mgi_->getActiveJoints().at(3); // "yumi_joint_3_r"
         joint_constraint.position = 45 * (M_PI / 180);
         joint_constraint.tolerance_above = 0.2;
         joint_constraint.tolerance_below = 0.2;
         joint_constraint.weight = 1.0;
         path_constraints.joint_constraints.push_back(joint_constraint);
         // Set the path constraints for the goal
-        move_group_interface_->setPathConstraints(path_constraints);
-        move_group_interface_->setGoalPositionTolerance(0.02);
+        arm_mgi_->setPathConstraints(path_constraints);
+        arm_mgi_->setGoalPositionTolerance(0.02);
 
         geometry_msgs::TransformStamped targetTransform;
         geometry_msgs::TransformStamped linkTransform;
@@ -229,7 +229,7 @@ public:
         targetTransform = transformListenerMsg.response.transformStamped;
 
         transformListenerMsg.request.target_frame = "yumi_base_link";
-        transformListenerMsg.request.source_frame = move_group_interface_->getLinkNames().at(1); //"yumi_link_2_r"
+        transformListenerMsg.request.source_frame = arm_mgi_->getLinkNames().at(1); //"yumi_link_2_r"
         transform_listener.call(transformListenerMsg);
         linkTransform = transformListenerMsg.response.transformStamped;
 
@@ -257,14 +257,14 @@ public:
         visual_tools_->publishAxis(final_pose);
         visual_tools_->trigger();
 
-        move_group_interface_->setPoseTarget(final_pose);
+        arm_mgi_->setPoseTarget(final_pose);
 
         moveit::planning_interface::MoveGroupInterface::Plan plan;
 
-        bool success = (move_group_interface_->plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        bool success = (arm_mgi_->plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-        move_group_interface_->move();
-        move_group_interface_->clearPathConstraints();
+        arm_mgi_->move();
+        arm_mgi_->clearPathConstraints();
     }
 
     // bool mode (true = screw, false = unscrew)
@@ -272,8 +272,8 @@ public:
     {
         double distance = 0.05;
 
-        double maxBound = kinematic_model_->getVariableBounds(move_group_interface_->getActiveJoints().at(6)).max_position_;
-        double minBound = kinematic_model_->getVariableBounds(move_group_interface_->getActiveJoints().at(6)).min_position_;
+        double maxBound = arm_mgi_->getRobotModel()->getVariableBounds(arm_mgi_->getActiveJoints().at(6)).max_position_;
+        double minBound = arm_mgi_->getRobotModel()->getVariableBounds(arm_mgi_->getActiveJoints().at(6)).min_position_;
 
         Eigen::Isometry3d unscrew_inital_eigen;
         visual_tools_->convertPoseSafe(input_pose, unscrew_inital_eigen);
@@ -284,19 +284,19 @@ public:
         geometry_msgs::Pose pose = mode ? unscrew_inital_pose : input_pose;
 
         std::vector<double> joint_group_positions;
-        moveit::core::RobotState start_state(*move_group_interface_->getCurrentState());
-        start_state.copyJointGroupPositions(joint_model_group_, joint_group_positions);
+        moveit::core::RobotState start_state(*arm_mgi_->getCurrentState());
+        start_state.copyJointGroupPositions(arm_jmg_, joint_group_positions);
 
         joint_group_positions.back() = mode ? minBound : maxBound;
-        move_group_interface_->setJointValueTarget(joint_group_positions);
+        arm_mgi_->setJointValueTarget(joint_group_positions);
 
         moveit::planning_interface::MoveGroupInterface::Plan plan_Set_Initial_Position;
 
-        bool success = (move_group_interface_->plan(plan_Set_Initial_Position) == moveit::core::MoveItErrorCode::SUCCESS);
-        move_group_interface_->move();
+        bool success = (arm_mgi_->plan(plan_Set_Initial_Position) == moveit::core::MoveItErrorCode::SUCCESS);
+        arm_mgi_->move();
 
         // Perform action of EndEffector
-        geometry_msgs::Pose start_end_effector_pose = move_group_interface_->getCurrentPose().pose;
+        geometry_msgs::Pose start_end_effector_pose = arm_mgi_->getCurrentPose().pose;
         Eigen::Isometry3d goal_end_effector_eigen;
 
         double turn_angle = mode ? M_PI_2 : -M_PI_2;
@@ -333,7 +333,7 @@ public:
 
             const double jump_threshold = 0.0;
             const double eef_step = 0.1;
-            fraction = move_group_interface_->computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+            fraction = arm_mgi_->computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
 
             visual_tools_->deleteAllMarkers();
 
@@ -351,28 +351,145 @@ public:
                 ROS_INFO_NAMED("tutorial", "Visualizing Cartesian path (%.2f%% achieved)", fraction * 100.0);
             }
         } while (fraction < 0.8);
-        move_group_interface_->execute(trajectory);
+        arm_mgi_->execute(trajectory);
     }
 
-    void pointTo(std::string object_id)
+    void pointTo(std::string object_id, string target_frame)
     {
+
+        gripper_mgi_->setNamedTarget("close");
 
         std::map<std::string, moveit_msgs::CollisionObject> objects = planning_scene_interface_->getObjects();
 
         visual_tools_->publishAxis(objects[object_id].pose);
         visual_tools_->trigger();
+
+        geometry_msgs::TransformStamped targetTransform;
+        geometry_msgs::TransformStamped linkTransform;
+
+        planner::TransformListener transformListenerMsg;
+
+        transformListenerMsg.request.target_frame = "yumi_base_link";
+        transformListenerMsg.request.source_frame = target_frame;
+        transform_listener.call(transformListenerMsg);
+        targetTransform = transformListenerMsg.response.transformStamped;
+
+        double xTarget = targetTransform.transform.translation.x - linkTransform.transform.translation.x;
+        double yTarget = targetTransform.transform.translation.y - linkTransform.transform.translation.y;
+
+        double dist_ratio = 0.3 / sqrt(pow(xTarget, 2) + pow(yTarget, 2));
+        double angle = atan2(yTarget, xTarget);
+
+        // The eef's final pose will always be upright and about 0.4 units high.
+
+        geometry_msgs::Pose final_pose;
+        final_pose.position.z = 0.40;
+
+        final_pose.position.x = linkTransform.transform.translation.x + xTarget * dist_ratio;
+        final_pose.position.y = linkTransform.transform.translation.y + yTarget * dist_ratio;
+
+        tf2::Quaternion q(tf2::Vector3(0, 0, 1), angle);
+
+        geometry_msgs::Quaternion q_final_pose_msg;
+
+        tf2::convert(q, q_final_pose_msg);
+        final_pose.orientation = q_final_pose_msg;
+
+        visual_tools_->publishAxis(final_pose);
+        visual_tools_->trigger();
     }
 
 private:
     ros::NodeHandle n_;
-    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface_;
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> arm_mgi_;
+    const moveit::core::JointModelGroup *arm_jmg_;
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> gripper_mgi_;
+    const moveit::core::JointModelGroup *gripper_jmg_;
     std::shared_ptr<moveit::planning_interface::PlanningSceneInterface> planning_scene_interface_;
-    const moveit::core::JointModelGroup *joint_model_group_;
     std::shared_ptr<moveit_visual_tools::MoveItVisualTools> visual_tools_;
 
     robot_model::RobotModelPtr kinematic_model_;
     ros::ServiceClient transform_listener;
 };
+
+std::vector<moveit_msgs::CollisionObject> addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &planning_scene_interface)
+{
+    std::vector<moveit_msgs::CollisionObject> collision_objects;
+    collision_objects.resize(3);
+
+    // Add the box
+    collision_objects[0].id = "box";
+    collision_objects[0].header.frame_id = "world";
+
+    /* Define the primitive and its dimensions. */
+    collision_objects[0].primitives.resize(1);
+    collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+    collision_objects[0].primitives[0].dimensions.resize(3);
+    collision_objects[0].primitives[0].dimensions[0] = 0.05;
+    collision_objects[0].primitives[0].dimensions[1] = 0.05;
+    collision_objects[0].primitives[0].dimensions[2] = 0.05;
+
+    /* Define the pose of the table. */
+    collision_objects[0].primitive_poses.resize(1);
+    collision_objects[0].primitive_poses[0].position.x = 0.53;
+    collision_objects[0].primitive_poses[0].position.y = -0.23;
+    collision_objects[0].primitive_poses[0].position.z = 0.025;
+    collision_objects[0].primitive_poses[0].orientation.x = 0;
+    collision_objects[0].primitive_poses[0].orientation.y = 0;
+    collision_objects[0].primitive_poses[0].orientation.z = 0;
+    collision_objects[0].primitive_poses[0].orientation.w = 1.0;
+
+    collision_objects[0].operation = collision_objects[0].ADD;
+
+    // Add the sphere
+    collision_objects[1].id = "sphere";
+    collision_objects[1].header.frame_id = "world";
+
+    // Define the sphere and its radius
+    collision_objects[1].primitives.resize(1);
+    collision_objects[1].primitives[0].type = collision_objects[1].primitives[0].SPHERE;
+    collision_objects[1].primitives[0].dimensions.resize(1); // Only one dimension for a sphere
+    collision_objects[1].primitives[0].dimensions[0] = 0.05; // Radius
+
+    // Define the pose of the sphere
+    collision_objects[1].primitive_poses.resize(1);
+    collision_objects[1].primitive_poses[0].position.x = 0.45;
+    collision_objects[1].primitive_poses[0].position.y = -0.34;
+    collision_objects[1].primitive_poses[0].position.z = 0.025;
+    collision_objects[1].primitive_poses[0].orientation.x = 0;
+    collision_objects[1].primitive_poses[0].orientation.y = 0;
+    collision_objects[1].primitive_poses[0].orientation.z = 0;
+    collision_objects[1].primitive_poses[0].orientation.w = 1.0;
+
+    collision_objects[1].operation = collision_objects[1].ADD;
+
+    // Add a cylinder
+    collision_objects[2].id = "cylinder";
+    collision_objects[2].header.frame_id = "world";
+
+    // Define the cylinder and its dimensions
+    collision_objects[2].primitives.resize(1);
+    collision_objects[2].primitives[0].type = collision_objects[2].primitives[0].CYLINDER;
+    collision_objects[2].primitives[0].dimensions.resize(2); // Two dimensions for a cylinder
+    collision_objects[2].primitives[0].dimensions[0] = 0.05; // Height
+    collision_objects[2].primitives[0].dimensions[1] = 0.05; // Radius
+
+    // Define the pose of the cylinder
+    collision_objects[2].primitive_poses.resize(1);
+    collision_objects[2].primitive_poses[0].position.x = 0.53;
+    collision_objects[2].primitive_poses[0].position.y = -0.06;
+    collision_objects[2].primitive_poses[0].position.z = 0.025;
+    collision_objects[2].primitive_poses[0].orientation.x = 0;
+    collision_objects[2].primitive_poses[0].orientation.y = 0;
+    collision_objects[2].primitive_poses[0].orientation.z = 0;
+    collision_objects[2].primitive_poses[0].orientation.w = 1.0;
+
+    collision_objects[2].operation = collision_objects[2].ADD;
+
+    planning_scene_interface.applyCollisionObjects(collision_objects);
+
+    return collision_objects;
+}
 
 int main(int argc, char **argv)
 {
@@ -381,15 +498,24 @@ int main(int argc, char **argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    static const std::string PLANNING_GROUP = "right_arm";
-    auto move_group_interface = std::make_shared<moveit::planning_interface::MoveGroupInterface>(PLANNING_GROUP);
-    // move_group_interface->setPlannerId("stomp");
-    move_group_interface->setPlanningTime(10.0);
-    move_group_interface->setMaxVelocityScalingFactor(1);
-
     auto planning_scene_interface = std::make_shared<moveit::planning_interface::PlanningSceneInterface>();
-    const moveit::core::JointModelGroup *joint_model_group =
-        move_group_interface->getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+    auto both_arms_mgi = std::make_shared<moveit::planning_interface::MoveGroupInterface>("both_arms");
+    const moveit::core::JointModelGroup *both_arms_jmg = both_arms_mgi->getCurrentState()->getJointModelGroup("both_arms");
+
+    auto right_arm_mgi = std::make_shared<moveit::planning_interface::MoveGroupInterface>("right_arm");
+    const moveit::core::JointModelGroup *right_arm_jmg = right_arm_mgi->getCurrentState()->getJointModelGroup("right_arm");
+
+    auto left_arm_mgi = std::make_shared<moveit::planning_interface::MoveGroupInterface>("left_arm");
+    const moveit::core::JointModelGroup *left_arm_jmg = left_arm_mgi->getCurrentState()->getJointModelGroup("left_arm");
+
+    auto right_gripper_mgi = std::make_shared<moveit::planning_interface::MoveGroupInterface>("right_gripper");
+    const moveit::core::JointModelGroup *right_gripper_jmg = right_gripper_mgi->getCurrentState()->getJointModelGroup("right_gripper");
+
+    auto left_gripper_mgi = std::make_shared<moveit::planning_interface::MoveGroupInterface>("left_gripper");
+    const moveit::core::JointModelGroup *left_gripper_jmg = left_gripper_mgi->getCurrentState()->getJointModelGroup("left_gripper");
+
+    right_arm_mgi->setPlannerId("stomp");
 
     namespace rvt = rviz_visual_tools;
     auto visual_tools = std::make_shared<moveit_visual_tools::MoveItVisualTools>("yumi_base_link");
@@ -402,12 +528,11 @@ int main(int argc, char **argv)
     visual_tools->publishText(text_pose, "NON VERBAL MOVEMENT TESTING", rvt::WHITE, rvt::XXLARGE);
     visual_tools->trigger();
 
-    HRI_Interface hri_interface(node_handle, move_group_interface, planning_scene_interface, joint_model_group, visual_tools);
+    addCollisionObjects(*planning_scene_interface);
 
-    move_group_interface->setNamedTarget("home");
-    move_group_interface->move();
+    HRI_Interface right_arm_hri(node_handle, right_arm_mgi, right_arm_jmg, right_gripper_mgi, right_gripper_jmg, planning_scene_interface, visual_tools);
 
-    geometry_msgs::Pose pose = move_group_interface->getCurrentPose().pose;
+    geometry_msgs::Pose pose = right_arm_mgi->getCurrentPose().pose;
 
     // for (const std::string &link_name : move_group_interface->getLinkNames())
     // {
@@ -437,10 +562,10 @@ int main(int argc, char **argv)
         {
         case 1:
             cout << "You chose to GO TO TARGET.\n";
-            hri_interface.yes_no("my_marker");
+            right_arm_hri.attention("my_marker");
             break;
         case 2:
-            hri_interface.screw_unscrew(mode, move_group_interface->getCurrentPose().pose);
+            right_arm_hri.screw_unscrew(mode, right_arm_mgi->getCurrentPose().pose);
             break;
         case 3:
             cout << "Enter 0 (unscrew) or 1 (screw): ";
@@ -461,7 +586,7 @@ int main(int argc, char **argv)
         case 4:
             cout << "Enter \"object id\" to point to:";
             cin >> input;
-            hri_interface.pointTo(input);
+            right_arm_hri.pointTo(input, "my_marker");
             break;
         case 5:
             cout << "Exiting the program...\n";
